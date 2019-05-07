e47b4ba90067cb211deac8bdd3044551ce4fc83a
John Sorrentino <sorrenj@sunyit.edu>
2019-05-06 21:11:05 -0400

    added minutes until eating to weight.py

--- python/weight.py
+++ python/weight.py
@@ -2,20 +2,18 @@ import time
 ### User entered data
 startDate = (2019, 4, 29, 13, 3, 39, 0, 119, 1)  #year, month, day
 endDate = (2019, 5, 29)  #year, month, day
-startingWeight = 172
-goalPerWeek = 2  #how many pounds you want to lose per week
+startingWeight = 172.4  #
+goalPerWeek = 3  #how many pounds you want to lose per week
 
 goalPerDay = goalPerWeek/7
 goalPerHour = goalPerDay/24
 
-buffer = 1/24
+buffer = 2/24
 
-currentWeight = int(input("Enter your current weight: "))
+currentWeight = float(input("Enter your current weight: "))
 
 currentDate = time.localtime()
 
-x = currentDate[3]
-print(x)
 
 requiredWeight = (startingWeight - (currentDate[7] - startDate[7])*goalPerDay) + buffer*currentDate[3] + (buffer/60)*currentDate[4]
 
@@ -23,3 +21,8 @@ if(currentWeight < requiredWeight):
     print("you can eat", currentWeight, requiredWeight)
 else:
     print("you can not eat", currentWeight, requiredWeight)
+
+
+i = (currentWeight - requiredWeight)/(buffer/60)
+
+print("You can eat in", i, "minutes")
--- test.py
+++
@@ -1,9 +0,0 @@
-i=str(input())
-while (i != '0'):
-	if (i == "muffin" and muffins > 0):
-		muffins -= 1
-	elif (i == "cupcake" and cupcake > 0):
-		cupcake -= 1
-	else:
-		print("Out of Stock")
-print("muffins:", muffins, "cupcakes:",cupcakes)
