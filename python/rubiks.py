8fb4182cd2fe1f8b5e81027e994ae0dc9eb91f5d
John Sorrentino <sorrenj@sunyit.edu>
2019-05-09 17:30:34 -0400

    combined cube.py and rubiks.py so now you only need to run rubiks.py

--- python/rubiks.py
+++ python/rubiks.py
@@ -2,6 +2,14 @@
 #tesitghgfhsdfasdffdshgsdfh
 import random
 import arcade
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import numpy
+
+
+
 """
 string printCube(string cube)
 string rightclock(string cube)
@@ -1201,5 +1209,600 @@ def main():
     print(cubeStates)
     print(moves)
 
+ 
+
 for i in range(1):
     main()
+    
+
+
+
+#################################   Start of 3d cube   ##################################
+
+cubeIndex = 0
+speed = 1
+bla = (0,0,0)
+g = (0,1,0)
+b = (0,0,1)
+r = (1,0,0)
+y = (1,1,0)
+o = (1,.5,0)
+w = (1,1,1)
+
+curMove = 0
+
+cubeStates.insert(0,cubeStates[0])
+cube = []
+cube = cubeStates[curMove]
+
+
+cubeColors = []
+
+for i in cube:
+    if i == 'r':
+        cubeColors.append(r)
+    elif i == 'w':
+        cubeColors.append(w)
+    elif i == 'g':
+        cubeColors.append(g)
+    elif i == 'y':
+        cubeColors.append(y)
+    elif i == 'o':
+        cubeColors.append(o)
+    elif i == 'b':
+        cubeColors.append(b)
+
+colors = []
+
+for i in range(27):
+    colors.append([bla,bla,bla,bla,bla,bla])
+
+class Cube:
+    global cubeIndex
+    x = 0
+    y = 0
+    z = 0
+    vertices = [
+        [0,0,0],
+        [0,0,1],
+        [0,1,0],
+        [0,1,1],
+        [1,0,0],
+        [1,0,1],
+        [1,1,0],
+        [1,1,1]
+        ]
+
+    edges = (
+        (0,1),
+        (0,2),
+        (0,4),
+        (1,3),
+        (1,5),
+        (2,3),
+        (2,6),
+        (3,7),
+        (4,5),
+        (4,6),
+        (5,7),
+        (6,7)
+    )
+
+    surfaces = (
+        (0,1,5,4),
+        (0,1,3,2),
+        (0,2,6,4),
+        (4,5,7,6),
+        (1,3,7,5),
+        (2,3,7,6)
+    )
+
+
+    # colors = [g,g,g,g,g,g]
+
+    def __init__(self, mul=1, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.edges = Cube.edges
+        self.vertices = list(numpy.multiply(numpy.array(Cube.vertices), mul))
+        self.vertices = list(map(lambda vert: (vert[0] + x, vert[1] + y, vert[2] + z), self.vertices))
+        self.surfaces = Cube.surfaces
+
+    def draw(self):
+        self.draw_sides()
+        glLineWidth(3)
+        glBegin(GL_LINES)
+        #glPushMatrix()
+        #glRotatef(45, 0, 0, 0)
+        
+        for edge in self.edges:
+            for vertex in edge:
+                glColor3f(0,0,0)
+                glVertex3fv(self.vertices[vertex])
+    
+        #glPopMatrix()
+        glEnd()
+
+    def draw_sides(self):
+        global cubeIndex
+        glBegin(GL_QUADS)
+        i = 0
+        for surface in self.surfaces:
+            for vertex in surface:
+                glColor3f(colors[cubeIndex][i][0],colors[cubeIndex][i][1],colors[cubeIndex][i][2])
+                glVertex3fv(self.vertices[vertex])
+            i += 1
+        cubeIndex += 1
+
+        glEnd()
+
+    def move(self, x, y, z):
+        self.vertices = list(map(lambda vert: (vert[0] + x, vert[1] + y, vert[2] + z), self.vertices))
+
+def yc3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.x == 3 and x >= -90:
+            glPushMatrix()
+            glTranslate(0, 2.5, 2.5)
+            glRotatef(x, 1, 0, 0)
+            glTranslate(0, -2.5, -2.5)
+            c.draw()
+            glPopMatrix()
+            if x == -90 and flag == 0:
+                curMove += 1
+                flag = 1
+                x = 0
+                updateColors()
+        else:
+            c.draw()
+    x += -speed
+    
+    return(x)
+
+def ycc3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.x == 3 and x <= 90:
+            glPushMatrix()
+            glTranslate(0, 2.5, 2.5)
+            glRotatef(x, 1, 0, 0)
+            glTranslate(0, -2.5, -2.5)
+            c.draw()
+            glPopMatrix()
+            if x == 90 and flag == 0:
+                curMove += 1
+                flag = 1
+                x = 0
+                updateColors()
+        else:
+            c.draw()
+    x += speed
+    
+    return(x)
+
+def wc3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.x == 1 and x <= 90:
+            glPushMatrix()
+            glTranslate(0, 2.5, 2.5)
+            glRotatef(x, 1, 0, 0)
+            glTranslate(0, -2.5, -2.5)
+            c.draw()
+            glPopMatrix()
+            if x == 90 and flag == 0:
+                curMove += 1
+                flag = 1
+                x = 0
+                updateColors()
+        else:
+            c.draw()
+    x += speed
+    return(x)
+
+def wcc3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.x == 1 and x >= -90:
+            glPushMatrix()
+            glTranslate(0, 2.5, 2.5)
+            glRotatef(x, 1, 0, 0)
+            glTranslate(0, -2.5, -2.5)
+            c.draw()
+            glPopMatrix()
+            if x == -90 and flag == 0:
+                curMove += 1
+                flag = 1
+                x = 0
+                updateColors()
+        else:
+            c.draw()
+    x += -speed
+    return(x)
+
+def gc3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.z == 3 and x >= -90:
+            glPushMatrix()
+            glTranslate(2.5, 2.5, 0)
+            glRotatef(x, 0, 0, 1)
+            glTranslate(-2.5, -2.5, 0)
+            c.draw()
+            glPopMatrix()
+            if x == -90 and flag == 0:
+                curMove += 1
+                flag = 1
+                x = 0
+                updateColors()
+        else:
+            c.draw()
+    x += -speed
+    return(x)
+
+def gcc3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.z == 3 and x <= 90:
+            glPushMatrix()
+            glTranslate(2.5, 2.5, 0)
+            glRotatef(x, 0, 0, 1)
+            glTranslate(-2.5, -2.5, 0)
+            c.draw()
+            glPopMatrix()
+            if x == 90 and flag == 0:
+                curMove += 1
+                flag = 1
+                x = 0
+                updateColors()
+        else:
+            c.draw()
+    x += speed
+    return(x)
+
+
+
+def bc3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.z == 1 and x <= 90:
+            glPushMatrix()
+            glTranslate(2.5, 2.5, 0)
+            glRotatef(x, 0, 0, 1)
+            glTranslate(-2.5, -2.5, 0)
+            c.draw()
+            glPopMatrix()
+            if x == 90 and flag == 0:
+                curMove += 1
+                flag = 1
+                x = 0
+                updateColors()
+        else:
+            c.draw()
+    x += speed
+    return(x)
+
+def bcc3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.z == 1 and x >= -90:
+            glPushMatrix()
+            glTranslate(2.5, 2.5, 0)
+            glRotatef(x, 0, 0, 1)
+            glTranslate(-2.5, -2.5, 0)
+            c.draw()
+            glPopMatrix()
+            if x == -90 and flag == 0:
+                curMove += 1
+                flag = 1
+                x = 0
+                updateColors()
+        else:
+            c.draw()
+    x += -speed
+    return(x)
+
+def rc3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.y == 3 and x >= -90:
+            glPushMatrix()
+            glTranslate(2.5, 0, 2.5)
+            glRotatef(x, 0, 1, 0)
+            glTranslate(-2.5, 0, -2.5)
+            c.draw()
+            glPopMatrix()
+            if x == -90 and flag == 0:
+                curMove += 1
+                flag = 1
+                x = 0
+                updateColors()
+        else:
+            c.draw()
+    x += -speed
+    return(x)
+def rcc3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.y == 3 and x <= 90:
+            glPushMatrix()
+            glTranslate(2.5, 0, 2.5)
+            glRotatef(x, 0, 1, 0)
+            glTranslate(-2.5, 0, -2.5)
+            c.draw()
+            glPopMatrix()
+            if x == 90 and flag == 0:
+                curMove += 1
+                flag = 1
+                x = 0
+                updateColors()
+        else:
+            c.draw()
+    x += speed
+    return(x)
+
+def oc3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.y == 1 and x <= 90:
+            glPushMatrix()
+            glTranslate(2.5, 0, 2.5)
+            glRotatef(x, 0, 1, 0)
+            glTranslate(-2.5, 0, -2.5)
+            c.draw()
+            glPopMatrix()
+            if x == 90 and flag == 0:
+                curMove += 1
+                cube = cubeStates[curMove]
+                flag = 1
+                x = 0
+                updateColors()
+
+        else:
+            c.draw()
+    x += speed
+    return(x)
+
+def occ3D(x , p):
+    global curMove, speed
+    flag = 0
+    for c in p:
+        if c.y == 1 and x >= -90:
+            glPushMatrix()
+            glTranslate(2.5, 0, 2.5)
+            glRotatef(x, 0, 1, 0)
+            glTranslate(-2.5, 0, -2.5)
+            c.draw()
+            glPopMatrix()
+            if x == -90 and flag == 0:
+                curMove += 1
+                flag = 1
+                x = 0
+                updateColors()
+        else:
+            c.draw()
+    x += -speed
+    return(x)
+
+def updateColors():
+    global cubeColors
+    for i in cube:
+        if i == 'r':
+            cubeColors.append(r)
+        elif i == 'w':
+            cubeColors.append(w)
+        elif i == 'g':
+            cubeColors.append(g)
+        elif i == 'y':
+            cubeColors.append(y)
+        elif i == 'o':
+            cubeColors.append(o)
+        elif i == 'b':
+            cubeColors.append(b)
+    #white side
+    colors[6][1] = cubeColors[9]
+    colors[7][1] = cubeColors[10]
+    colors[8][1] = cubeColors[11]
+    colors[3][1] = cubeColors[12]
+    colors[4][1] = cubeColors[13]
+    colors[5][1] = cubeColors[14]
+    colors[0][1] = cubeColors[15]
+    colors[1][1] = cubeColors[16]
+    colors[2][1] = cubeColors[17]
+
+    #yellow side
+    for i in range(27):
+        if i >= 18:
+            colors[i][3] = cubeColors[53 - i]
+
+    #green side
+    colors[2][4] = cubeColors[24]
+    colors[5][4] = cubeColors[21]
+    colors[8][4] = cubeColors[18]
+    colors[11][4] = cubeColors[25]
+    colors[14][4] = cubeColors[22]
+    colors[17][4] = cubeColors[19]
+    colors[20][4] = cubeColors[26]
+    colors[23][4] = cubeColors[23]
+    colors[26][4] = cubeColors[20]
+
+    #red side
+    colors[6][5] = cubeColors[0]
+    colors[7][5] = cubeColors[3]
+    colors[8][5] = cubeColors[6]
+    colors[15][5] = cubeColors[1]
+    colors[16][5] = cubeColors[4]
+    colors[17][5] = cubeColors[7]
+    colors[24][5] = cubeColors[2]
+    colors[25][5] = cubeColors[5]
+    colors[26][5] = cubeColors[8]
+
+    #orange side
+    colors[0][0] = cubeColors[42]
+    colors[1][0] = cubeColors[39]
+    colors[2][0] = cubeColors[36]
+    colors[9][0] = cubeColors[43]
+    colors[10][0] = cubeColors[40]
+    colors[11][0] = cubeColors[37]
+    colors[18][0] = cubeColors[44]
+    colors[19][0] = cubeColors[41]
+    colors[20][0] = cubeColors[38]
+
+    #blue side
+    colors[0][2] = cubeColors[45]
+    colors[3][2] = cubeColors[48]
+    colors[6][2] = cubeColors[51]
+    colors[9][2] = cubeColors[46]
+    colors[12][2] = cubeColors[49]
+    colors[15][2] = cubeColors[52]
+    colors[18][2] = cubeColors[47]
+    colors[21][2] = cubeColors[50]
+    colors[24][2] = cubeColors[53]
+
+    cubeColors.clear()
+    cubeColors = []
+
+
+def cube3d():
+    global cubeIndex, curMove, moves,cube,cubeColors
+    pygame.init()
+    display = (800,800)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50)
+
+    glTranslatef(0, 3,-20)
+    glRotate(20,1,0,0)
+    glEnable(GL_DEPTH_TEST)
+
+    p = []
+
+#makes all of the smaller cubes
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                p.append(Cube(1,(i+1),(j+1),(k+1)))
+
+#trying to make the colors only on the sides when solved
+    # for c in p:
+    #     if c.x == 3:
+    #         print(True)
+    #         c.colors = (p[0].b,p[0].w,p[0].w,p[0].w,p[0].w,p[0].w,p[0].w)
+    #p[26].colors = (bla, bla, w, cubeColors[27], cubeColors[20], cubeColors[8], bla)
+
+    
+
+
+    vel = 4
+    clock = pygame.time.Clock()
+    x = 0
+    viewAngle = 0
+    glTranslate(-2.5, -2.5, 7)
+    updateColors()
+#start of while loop
+    while True:
+        cubeIndex = 0
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        keys = pygame.key.get_pressed()
+        # if keys[pygame.K_LEFT]:
+        #     glTranslatef(-vel, 0, 0)
+        # if keys[pygame.K_RIGHT]:
+        #     glTranslatef(vel, 0, 0)
+        # if keys[pygame.K_UP]:
+        #     glTranslatef(0, vel, 0)
+        # if keys[pygame.K_DOWN]:
+        #     glTranslatef(0, -vel, 0)
+        # if keys[pygame.K_t]:
+        #     glTranslatef(0, 0, vel)
+        # if keys[pygame.K_g]:
+        #     glTranslatef(0, 0, -vel)
+        if keys[pygame.K_d]:
+            glTranslate(2.5, 2.5, 2.5)
+            glRotatef(vel, 0,1,0)
+            glTranslate(-2.5, -2.5, -2.5)
+        if keys[pygame.K_a]:
+            glTranslate(2.5, 2.5, 2.5)
+            glRotatef(-vel, 0,1,0)
+            glTranslate(-2.5, -2.5, -2.5)
+        if keys[pygame.K_w]:
+            glTranslate(2.5, 2.5, 2.5)
+            glRotatef(-vel, 1,0,0)
+            glTranslate(-2.5, -2.5, -2.5)
+        if keys[pygame.K_s]:
+            glTranslate(2.5, 2.5, 2.5)
+            glRotatef(vel, 1,0,0)
+            glTranslate(-2.5, -2.5, -2.5)
+        if keys[pygame.K_q]:
+            glTranslate(2.5, 2.5, 2.5)
+            glRotatef(vel, 0,0,1)
+            glTranslate(-2.5, -2.5, -2.5)
+        if keys[pygame.K_e]:
+            glTranslate(2.5, 2.5, 2.5)
+            glRotatef(-vel, 0,0,1)
+            glTranslate(-2.5, -2.5, -2.5)
+        if keys[pygame.K_y]:
+            x = 0
+        if keys[pygame.K_n]:
+            curMove += 1
+
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        #x = 0
+        #glRotatef(x, 1.0, 0.0, 1.0)
+#start of moves
+        print(curMove)
+        if curMove < len(moves) and moves[curMove] == "yc":
+            x = yc3D(x, p)
+        elif curMove < len(moves) and moves[curMove] == "ycc":
+            x = ycc3D(x, p)
+        elif curMove < len(moves) and moves[curMove] == "wc":
+            x = wc3D(x, p)
+        elif curMove < len(moves) and moves[curMove] == "wcc":
+            x = wcc3D(x, p)
+        elif curMove < len(moves) and moves[curMove] == "gc":
+            x = gc3D(x, p)
+        elif curMove < len(moves) and moves[curMove] == "gcc":
+            x = gcc3D(x, p)
+        elif curMove < len(moves) and moves[curMove] == "rc":
+            x = rc3D(x, p)
+        elif curMove < len(moves) and moves[curMove] == "rcc":
+            x = rcc3D(x, p)
+        elif curMove < len(moves) and moves[curMove] == "bc":
+            x = bc3D(x, p)
+        elif curMove < len(moves) and moves[curMove] == "bcc":
+            x = bcc3D(x, p)
+        elif curMove < len(moves) and moves[curMove] == "oc":
+            x = oc3D(x, p)
+        elif curMove < len(moves) and moves[curMove] == "occ":
+            x = occ3D(x, p)
+        else:
+            for c in p:
+                c.draw()
+        
+        pygame.display.flip()
+
+        
+        
+        if curMove < len(cubeStates) - 2:
+            cube = cubeStates[curMove+2]
+            print(cubeStates[curMove])
+            print(cube)
+
+cube3d()
